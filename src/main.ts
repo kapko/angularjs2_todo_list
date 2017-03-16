19719981ea295a9779ffe1254deb417354c19e5b
Kapko <kapko19@mail.ru>
2017-03-16 22:33:49 +0700

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { Component, NgModule  } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
-import {HomeModule} from './home/home.module.ts'
-import {CommonModule} from '@angular/common'
-
 @Component({
 	selector: 'app', 
-	template: `<h1>hello World</h1> <home></home>`
+	template: `<h1>hello World</h1>`
 })
 
 export class AppComponent{}
@@ -14,8 +11,6 @@ export class AppComponent{}
 @NgModule({
 	imports: [ 
 		BrowserModule,
-		CommonModule,
-		HomeModule, 
 	],
 	declarations: [ AppComponent ],
 	bootstrap: [ AppComponent ]
